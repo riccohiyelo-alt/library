926d930aaa26b3d10512eb620bb1a734c0d31fdb
riccohiyelo-alt <riccohiyelo@gmail.com>
2026-02-24 17:01:03 +0300

    Update Atlanta.lua

--- Atlanta.lua
+++ Atlanta.lua
@@ -1634,11 +1634,11 @@
 					image = "rbxassetid://115194686863276",
 				})
 
-				local watermark = library:watermark({default = os.date('Atlanta |  - %b %d %Y - %H:%M:%S')})  
+				local watermark = library:watermark({default = os.date('NeverPaste |  - %b %d %Y - %H:%M:%S')})  
 
 				task.spawn(function()
 					while task.wait(1) do 
-						watermark.change_text(os.date('Atlanta - Beta - %b %d %Y - %H:%M:%S'))
+						watermark.change_text(os.date('NeverPaste - ALPHA - %b %d %Y - %H:%M:%S'))
 					end 
 				end) 
 
